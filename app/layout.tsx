01e684ff7f9ff6cb77236cabb05481ca9321f477
daniel-dieguez <danguez2001@gmail.com>
2026-04-03 12:07:16 -0600

    [UPDATE] Modulo layOut, se agrego codigo para google analitys

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; 
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,18 +20,37 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning // esto es apra que no me de errores de renderizado en el cliente
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col" 
-      suppressHydrationWarning // esto es apra que no me de errores de renderizado en el cliente
-      >{children}</body>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HQNESDJYD3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HQNESDJYD3');
+          `}
+        </Script>
+      </head>
+
+      <body
+        className="min-h-full flex flex-col"
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
     </html>
   );
 }
